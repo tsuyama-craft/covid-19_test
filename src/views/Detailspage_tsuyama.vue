4c405f87814b1ba15ff3fc4f6969e335951a1aa3
tsuyama-craft <82013625+kana0hi3@users.noreply.github.com>
2021-10-21 10:41:26 +0900

    Update Detailspage_tsuyama.vue

--- src/views/Detailspage_tsuyama.vue
+++ src/views/Detailspage_tsuyama.vue
@@ -4,8 +4,6 @@
     <router-link to="/">新型コロナウイルス情報</router-link><a>＞津山市コロナ感染者詳細情報</a>
   </div>
   <h1>津山市コロナ感染者詳細情報</h1>
-  <button type="button" v-on:click="Back" :disabled="isTestDisabledBack">前</button>
-  <button type="button" v-on:click="Next" :disabled="isTestDisabledNext">次</button>
   <ChartBar></ChartBar>
   <!--
   <canvas v-if="isLarge == true" id="chart" height="100" width="300"></canvas>
@@ -27,25 +25,5 @@ export default {
 </script>
 
 <style>
-h1{
-  position: relative;
-  color: #6eb0f9;
-  line-height: 1.4;
-  -webkit-box-reflect: below -10px -webkit-linear-gradient(top,rgba(0,0,0,0),rgba(0,0,0,0) 10%,rgba(0, 0, 0, 0.6));
-  margin: 30px 0;
-  font-size: 24px;
-}
-.pagelink{
-  text-align: left;
-  margin: 10px 0;
-  font-size: 10px;
-}
-@media screen and (min-width: 480px){
-  h1{
-    font-size: 36px;
-  }
-  .pagelink{
-    font-size: 14px;
-  }
-}
+
 </style>
