82d0f551eef457e3605826b9dbd96c6938010c19
tsuyama-craft <82013625+kana0hi3@users.noreply.github.com>
2021-10-26 10:39:28 +0900

    Revert "Update service-worker.js"
    This reverts commit cae3ecf4cee785bb816e9697f8ed45f4972b3d26.

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,10 +14,27 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.30a974b3945b821296701afad7e5e436.js"
+  "precache-manifest.68804490713d87374dec13e75d0dd140.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "craft"});
+workbox.core.setCacheNameDetails({ prefix: "craft" });
+
+self.addEventListener('activate', (event) => {
+  var cacheWhitelist = ['v2'];
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          // ホワイトリストにないキャッシュ(古いキャッシュ)は削除する
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
